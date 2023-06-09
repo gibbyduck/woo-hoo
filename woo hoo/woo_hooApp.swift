3ea3a4661a523202956de93ad1be5874ba81b858
Genevieve Bills <gigicarlabills@gmail.com>
2023-06-09 10:17:31 -0500

    Initial Commit

---
+++ woo hoo/woo_hooApp.swift
@@ -0,0 +1,17 @@
+//
+//  woo_hooApp.swift
+//  woo hoo
+//
+//  Created by Scholar on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct woo_hooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
